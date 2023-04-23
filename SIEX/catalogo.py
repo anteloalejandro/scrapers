240f67115891d7cfb370e6e00f7bc0f3d615d63f
Alejandro Antelo <alfasant@protonmail.com>
2023-04-23 12:23:49 +0200

    directorios separados para grupos de archivos

--- SIEX/catalogo.py
+++ SIEX/catalogo.py
@@ -30,21 +30,22 @@ cerrar_selector =   'p-dialog button.p-dialog-header-close'
 class CatalogoSpider(scrapy.Spider):
     name = 'catalogo_spider'
     start_urls = [ url ]
+    last_file_list = []
 
     # Inicializar webdriver
     def __init__(self):
 
         # Directorio de descarga
         script_name = os.path.basename(os.path.splitext(__file__)[0])
-        download_dir = os.path.dirname(__file__) + '/downloads/' + script_name
-        if not os.path.isdir(download_dir):
-            os.makedirs(download_dir)
+        self.download_dir = os.path.dirname(__file__) + '/downloads/' + script_name
+        if not os.path.isdir(self.download_dir):
+            os.makedirs(self.download_dir)
 
         # Configuración de firefox
         firefox_options = FirefoxOptions()
         if firefox_options:
             firefox_options.set_preference("browser.download.folderList", 2)
-            firefox_options.set_preference("browser.download.dir", download_dir)
+            firefox_options.set_preference("browser.download.dir", self.download_dir)
 
         # Firefox
         service = FirefoxService(GeckoDriverManager().install())
@@ -93,21 +94,72 @@ class CatalogoSpider(scrapy.Spider):
         elements = self.find(selector, parent)
         return elements[0] if len(elements) > 0 else None
 
+    def moveLast(self, dir):
+        max_tries = 100
+        sleep_time = .5
+
+        # Directorio en el que se guardará
+        target_dir = self.download_dir + '/' + dir
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+
+        # Sacar archivos en la carpeta de descargas
+        files = []
+        last_file = ''
+
+        found_file = False
+
+        while max_tries > 0:
+            max_tries -= 1
+            sleep(sleep_time)
+
+            # Obtener lista de ficheros
+            files = [f for f in os.listdir(self.download_dir) if os.path.isfile(os.path.join(self.download_dir, f))]
+            files.sort(key = lambda x: os.stat(self.download_dir + '/' + x).st_mtime)
+
+            # Comprobar si se ha descargado
+            last_file = files[len(files) - 1]
+            if last_file not in self.last_file_list:
+                found_file = True
+                break
+
+        # Si no se ha descargado, para
+        if not found_file:
+            self.last_file_list = files
+            return False
+
+        # Borrar archivo de la lista antes de guardarla
+        files.remove(last_file)
+        self.last_file_list = files
+
+        # Mover archivo
+        old_path = self.download_dir + '/' + last_file
+        new_path = target_dir + '/' + last_file
+        os.rename(old_path, new_path)
+
+        return True
+
+
     # Recorre la página y ejecuta el código principal
     def parse(self, response):
         self.driver.get(response.url)
 
         sleep(1)
         elements = self.find(list_selector)
+        count = 0
         for element in elements:
             element.click()
             links = self.find(link_selector, element)
+
+            count += 1
 
             for link in links:
                 link.click()
 
                 boton = self.findOne(boton_selector)
-                if (boton): boton.click()
+                if (boton):
+                    boton.click()
+                    self.moveLast(str(count))
 
                 cerrar = self.findOne(cerrar_selector)
                 if (cerrar): cerrar.click()
